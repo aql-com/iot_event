323c2043bd2c38a38c4447be3e05e5e97400e844
Andrew Morris <amorris@ad.aql.com>
2024-09-13 14:44:37 +0100

    Prepare for Event

--- Hackathon Materials/Previous Events/aql- Nexus Hackathon 2024/Code/Tier 2/Challenge 6/hackathon.js
+++ Hackathon Materials/Previous Events/aql- Nexus Hackathon 2024/Code/Tier 2/Challenge 6/hackathon.js
@@ -1,10 +1,10 @@
 const mqtt = require('mqtt');
-const LOCATION_ID = "QmRF9nRY0j";
-const DEVICE_ID = "JAqokSNQx52";
+const LOCATION_ID = "REPLACE_WITH_YOUR_LCOATON";
+const DEVICE_ID = "REPLACE_WITH_YOUR_DEVICE";
 const TOPIC = `location/${LOCATION_ID}/device/${DEVICE_ID}/event/up`;
-const TEAM = "Team 1"
-const USER_NAME = "hackathon"
-const PASSWORD  = "tAhZ2B85e3"
+const TEAM = "REPLACE_WITH_YOUT_TEAM_NAME"
+const USER_NAME = "PROVIDED AT THE EVENT"
+const PASSWORD  = "PROVIDED AT THE EVENT"
 
 let mqttConnectionDetails = {
     host:                       "mqtt.core.aql.com",
@@ -29,7 +29,7 @@ function generatePayload() {
     const latitude = Math.random() * (90 - (-90)) + (-90); // Random latitude between -90 and 90
     const longitude = Math.random() * (180 - (-180)) + (-180); // Random longitude between -180 and 180
     const temperature = Math.random() * (40 - (-20)) + (-20); // Random temperature between -20 and 40
-    const humidity = Math.random() * (100 - 0) + 0; // Random humidity between 0 and 100
+    const humidity = Math.random() * 100 - 0 + 0; // Random humidity between 0 and 100
     const co2 = Math.random() * (1000 - 400) + 400; // Random CO2 between 400 and 1000
     const pm25 = Math.random() * (100 - 0) + 0; // Random PM2.5 between 0 and 100
     const pm10 = Math.random() * (100 - 0) + 0; // Random PM10 between 0 and 100
